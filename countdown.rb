c7ead42668c5bd22bcc3b968e4f02d471d87eb71
Nathan Glick <nateleighglick@hotmail.com>
2020-10-26 12:19:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,9 @@
 #write your code here
 countdown_output == 10
 
-def countdown
+def countdown(countdown_output)
+  counter == 10
+  while countdown_output > 0
+  counter -= 1
+  puts "HAPPY NEW YEAR!"
 end
